46ce6c96a1dace0e830f15188aa19208237e1d3b
Saman <keensam04@gmail.com>
2020-04-18 13:34:26 +0530

    added sysout

--- src/main/java/com/ufh/Main.java
+++ src/main/java/com/ufh/Main.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Server started!!!");
         SpringApplication.run(Main.class,args);
 
     }
